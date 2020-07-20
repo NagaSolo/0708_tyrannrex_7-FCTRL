df1d95a909b9a9ce84c4d3e4476aaa0aad74237b
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-20 09:33:09 +0100

    add: brainstorming session

---
+++ fctrl.py
@@ -0,0 +1,13 @@
+''' given N number, count the zeros of its factorial output
+
+    input:
+    T -> testcases, 0 <= T <= 10_000
+    N -> number, 1 <= N <= 1_000_000_000
+
+    processor:
+    function Z(N) -> count the zeros of factorial N output
+
+    output:
+    non-negative Z(N)
+
+'''
